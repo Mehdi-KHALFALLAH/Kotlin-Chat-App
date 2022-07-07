b8a634879ce92d9bad09674c343c72b7ef58cfbb
Mehdi Khalfallah <contact.mehdi.khalfallah@gmail.com>
2022-07-07 17:16:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_chat_app/Login.kt
@@ -0,0 +1,34 @@
+package com.example.kotlin_chat_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class Login : AppCompatActivity() {
+
+    private  lateinit var edtEmail : EditText
+    private  lateinit var edtPassword : EditText
+    private  lateinit var btnLogin : Button
+    private  lateinit var btnSignUp: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignup)
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, Signup::class.java)
+            startActivity(intent)
+
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/kotlin_chat_app/Signup.kt
@@ -0,0 +1,14 @@
+package com.example.kotlin_chat_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Signup : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
