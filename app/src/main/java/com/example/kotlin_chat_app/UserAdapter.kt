3eb3a7246a1a368e10d14a0f868c8642dd704a66
Mehdi Khalfallah <contact.mehdi.khalfallah@gmail.com>
2022-08-12 15:02:43 +0100

    chat app 100% complete

--- app/src/main/java/com/example/kotlin_chat_app/UserAdapter.kt
+++ app/src/main/java/com/example/kotlin_chat_app/UserAdapter.kt
@@ -1,12 +1,14 @@
 package com.example.kotlin_chat_app
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 
 class UserAdapter(val context: Context, val userList : ArrayList<User>):
     RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
@@ -22,6 +24,14 @@ val view: View = LayoutInflater.from(context).inflate(R.layout.user_layout, pare
         val currentUser = userList[position]
 
         holder.textName.text = currentUser.name
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, ChatActivity::class.java)
+
+            intent.putExtra("name", currentUser.name)
+            intent.putExtra("uid", currentUser.uid)
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
